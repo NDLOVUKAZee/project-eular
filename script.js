0d4161420160521b5d75d7491ea6b1d14eb59459
nomthandazo <ndlovunomthandazo4@gmail.com>
2024-05-06 14:14:01 +0200

    Winning

--- script.js
+++ script.js
@@ -16,22 +16,51 @@ function firstFun() {
     
 }
 
-
 //2nd Problem
 const textTwo = document.getElementById("secBox");
 const buttonTwo = document.getElementById("secBtn");
 const outpTwo = document.getElementById("secOut");
 function secFun() {
-    
-    outpTwo.innerHTML = "The sum of these multiples is " + textTwo.value;
-}
+    let sum = 0;
+    let prev = 1;
+    let curr = 2;
 
+    while (curr <= textTwo.value) {
+        if (curr % 2 === 0) {
+            sum += curr;
+        }
+        let temp = curr;
+        curr += prev;
+        prev = temp;
+    }
+
+    outpTwo.innerHTML = "The sum of the even-valued terms: " + sum;
+}
 
 //3rd Problem
 const textThree = document.getElementById("thirdBox");
 const buttonThree = document.getElementById("thirdBtn");
 const outpThree = document.getElementById("thirdOut");
-function thirdFun() {
+function thirdFun(num) {
+    let largestPrime = 1;
     
-    outpThree.innerHTML = "The sum of these multiples is " + textThree.value;
+        while (num % 2 === 0) {
+        largestPrime = 2;
+        num = Math.floor(num / 2);
+    }
+    
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+        while (num % i === 0) {
+            largestPrime = i;
+            num = Math.floor(num / i);
+        }
+    }
+    
+    if (num > 2) {
+        largestPrime = num;
+    }
+
+   
+    outpThree.innerHTML = "The largest prime factor of the inserted number is " + largestPrime;
+
 }
